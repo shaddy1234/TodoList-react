365b87951f6af73790413a1a30f378cd2923c026
Shadrack Kimaau <musyokishadrach1@gmail.com>
2023-11-17 23:33:14 +0300

    initial set up

---
+++ todolist/src/components/Todo.jsx
@@ -0,0 +1,16 @@
+import React, { useEffect } from 'react'
+
+function Todo() {
+
+    const [todo, setTodo] = useEffect({
+        TodoName: "",
+        DueDate: ""
+    })
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Todo
